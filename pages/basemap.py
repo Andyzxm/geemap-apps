81f05d5bdc4651e9c90f677842f1292bf590c1b5
Xinming Andy Zhang <andyzxm1101@gmail.com>
2024-05-02 19:21:54 -0400

    update

--- pages/basemap.py
+++ pages/basemap.py
@@ -1,23 +1,19 @@
 import streamlit as st
-import watergeo.foliumap as watergeo
+import geemap.foliumap as geemap
 
-st.set_page_config(layout="wide")
+st.title("Interactive Map")
 
-markdown = """
-Web App URL: <https://watergeo.streamlit.app/>
-GitHub Repository: <https://github.com/Andyzxm/webmap_template>
-"""
+col1, col2 = st.columns([4, 1])
+options = list(geemap.basemaps.keys())
+index = options.index("OpenTopoMap")
 
-st.sidebar.title("About")
-st.sidebar.info(markdown)
-logo = "https://images-platform.99static.com/7WcMmZPzGbVHYpeaib5FcOYR314=/100x100:900x900/500x500/top/smart/99designs-contests-attachments/122/122380/attachment_122380314"
-st.sidebar.image(logo)
+with col2:
 
-st.title("watergeo map")
+    basemap = st.selectbox("Select a basemap:", options, index)
 
-with st.expander("See source code"):
-    with st.echo():
-        m = watergeo.Map()
-        m.add_basemap("OpenTopoMap")
 
-m.to_streamlit(height=700)
+with col1:
+
+    m = geemap.Map()
+    m.add_basemap(basemap)
+    m.to_streamlit(height=700)
